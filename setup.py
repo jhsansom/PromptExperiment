614b661cf9c823a41267f0f650e100a97c70dd57
demo <jhsansom@umich.edu>
2024-05-15 11:40:51 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GPT Experiment',
+      version='1.0',
+      description='Simple Infrastructure for Running Prompting Experiments on GPT',
+      author='Jake Sansom',
+      author_email='jhsansom@umich.edu',
+      py_modules = ['gpt_experiment']
+     )
